bda8884fc31b4f100c321b38251a713c3e93ee85
penaloza-pablo <72697527+penaloza-pablo@users.noreply.github.com>
2025-08-05 10:55:46 +0200

    basic lambda

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,12 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { myFirstFunction } from './my-first-function/resource';
 // import { fetchGuestyBookings } from './function/fetchGuestyBookings/resource';
 
 defineBackend({
   auth,
   data,
+  myFirstFunction,
   // fetchGuestyBookings,
 });
